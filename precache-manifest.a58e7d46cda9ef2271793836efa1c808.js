47bd5441396fd115f22c01c9b6acfa233ba78c0a
arrtechnology <arrtechnologysolutions@gmail.com>
2020-05-05 19:52:49 -0400

    Updates

--- precache-manifest.a58e7d46cda9ef2271793836efa1c808.js
+++ precache-manifest.a58e7d46cda9ef2271793836efa1c808.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69315826129a17fca954b1a796f45543",
+    "revision": "e8dbb03657ff9e9ffd3333abec586e05",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "82663970cebfee7173bf",
+    "revision": "67a394dc85ea9562d7ea",
     "url": "/portfolio/static/css/main.b5f9e209.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.8d514f31.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "82663970cebfee7173bf",
-    "url": "/portfolio/static/js/main.291e8a8f.chunk.js"
+    "revision": "67a394dc85ea9562d7ea",
+    "url": "/portfolio/static/js/main.3685c5e4.chunk.js"
   },
   {
     "revision": "f619aeccc94a0d089131",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ea4e3ae032379742a4e93be52643391f.js"
+  "/portfolio/precache-manifest.a58e7d46cda9ef2271793836efa1c808.js"
 );
 
 self.addEventListener('message', (event) => {
