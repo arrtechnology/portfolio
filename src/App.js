e395f08ff046f705eea947b11998decea68c8b17
arrtechnology <64655579+arrtechnology@users.noreply.github.com>
2020-05-05 17:02:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("inside app");
+  console.log("inside apps");
   return (
     <div className="App">
       <header className="App-header">
